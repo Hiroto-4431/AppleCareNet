f96326db5a4dd45158892207a599c4ede009248d
Hiroto <hirotomyk8995@gmail.com>
2022-09-22 10:40:26 +0900

    create HeaderView of PC

--- header.php
+++ header.php
@@ -5,15 +5,15 @@
   <!-- サイト情報 -->
   <meta charset="utf-8" />
   <meta http-equiv="x-ua-compatible" content="ie=edge" />
-  <title>タイトル</title>
-  <meta name="description" content="ディスクリプション" />
+  <title>アップルケアネット</title>
+  <meta name="description" content="アップルケアネット" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="format-detection" content="telephone=no" />
 
   <!-- OG -->
   <meta property="og:type" content="website" />
-  <meta property="og:title" content="タイトル" />
-  <meta property="og:description" content="ディスクリプション" />
+  <meta property="og:title" content="アップルケアネット" />
+  <meta property="og:description" content="アップルケアネット" />
   <meta property="og:image" content=" サムネイル画像の URL" />
 
   <!-- favicon -->
@@ -22,7 +22,9 @@
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" />
 
   <!-- GoogleFonts -->
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
   <!-- font awesome -->
   <script src="https://kit.fontawesome.com/0c7aed2221.js" crossorigin="anonymous"></script>
@@ -33,4 +35,35 @@
 
 <body>
   <!-- ===== ヘッダー ===== -->
-  <header class="header"></header>
+  <header class="l-header">
+    <!-- ヘッダーロゴ -->
+    <div class="l-header__logo">
+      <a href="#">
+        <img src="./assets/img/common/pc-header-logo.png" alt="">
+      </a>
+    </div>
+
+    <!-- ハンバーガメニュー ボタン -->
+    <div class="l-hmg__btn">
+      <button class="l-hmg__btn__content">
+        <div class="l-hmg__btn__content--wrapper">
+          <span class="l-hmg__btn__content--line"></span>
+          <span class="l-hmg__btn__content--line"></span>
+          <span class="l-hmg__btn__content--line"></span>
+        </div>
+      </button>
+    </div>
+
+    <!-- ハンバーガーメニュー コンテンツ -->
+
+
+    <!-- お問い合わせ -->
+    <div class="l-header__contact">
+      <a href="#">
+        <div class="l-header__contact__text">
+          <p class="c-text__header__contact--main">お問い合わせ</p>
+          <p class="c-text__header__contact--sub">CONTACT</p>
+        </div>
+      </a>
+    </div>
+  </header>
