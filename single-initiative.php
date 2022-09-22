158aa194b291b4aa32857de038f796a1cbde8537
Hiroto <hirotomyk8995@gmail.com>
2022-09-22 17:25:07 +0900

    create single InitiativePage

---
+++ single-initiative.php
@@ -0,0 +1,39 @@
+<!-- ===== アップルケアネットの取り組み[詳細] ===== -->
+<?php include('./header.php'); ?>
+<!-- ===== メイン ===== -->
+<main class="l-main l-single-initiative">
+  <section class="l-top">
+    <div class="l-top__main-text">
+      <h1 class="c-text__common__main-title">健康サポート薬局</h1>
+      <p class="c-text__common__sub-title">HEALTH SUPPORT</p>
+    </div>
+  </section>
+  <section class="l-content">
+    <div class="l-content__img">
+
+      <!-- <img src="./assets/img/common/pc-initiative01.png" alt=""> -->
+      <picture>
+        <source srcset="./assets/img/common/pc-initiative01.png" media="(min-width: 960px)" />
+        <source srcset="./assets/img/common/sp-initiative01.png" media="(max-width: 960px)" />
+        <img src="./assets/img/common/pc-initiative01.png" alt="aaa" />
+      </picture>
+    </div>
+    <div class="l-content__title">
+      <p class="c-text__single__title">患者さまにとって最も身近な存在へ</p>
+      <p class="c-text__single__sub-title">HEALTH SUPPORT</p>
+    </div>
+    <div class="l-content__post">
+      <p class="c-text__single__post">アップル薬局では、地域の患者さまに日々の健康と安心をお届けするために、<br>
+        最新調剤機器を用いた迅速かつ正確な調剤を心掛けています。<br>
+        また来局による調剤のみにとどまらず、在宅訪問による管理薬剤指導や、<br>
+        患者さまの状況に合わせた服薬の説明・相談・管理にもお応えしています。<br>
+        さらに、管理栄養士と連携することで「食」に関するサービス提供も行っています。</p>
+    </div>
+    <div class="l-content__back">
+      <a href="#">
+        <span class="c-text__common__back">一覧に戻る</span>
+      </a>
+    </div>
+  </section>
+</main>
+<?php include('./footer.php'); ?>
