99f506553eb223cebdca87da9950e76d1cd4d0eb
Hiroto <hirotomyk8995@gmail.com>
2022-09-26 15:53:42 +0900

    create AboutUs

---
+++ aboutUs.php
@@ -0,0 +1,235 @@
+<!-- ===== アップルケアネットについて ===== -->
+<?php include('./header.php'); ?>
+<!-- ===== メイン ===== -->
+<main class="l-main l-aboutUs">
+  <section class="l-top">
+    <div class="l-top__main-text">
+      <h1 class="c-text__common__main-title">私たちについて</h1>
+      <p class="c-text__common__sub-title">ABOUT US</p>
+      <div class="l-top__content lg-only">
+        <ul class="l-top__content__list">
+          <li class="c-text__common__content"><a href="#">アップルケアネットとは</a></li>
+          <li class="c-text__common__content">/</li>
+          <li class="c-text__common__content"><a href="#">代表メッセージ</a></li>
+          <li class="c-text__common__content">/</li>
+          <li class="c-text__common__content"><a href="#">企業理念</a></li>
+        </ul>
+        <ul class="l-top__content__list">
+          <li class="c-text__common__content"><a href="#">会社概要</a></li>
+          <li class="c-text__common__content">/</li>
+          <li class="c-text__common__content"><a href="#">グループ会社</a></li>
+          <li class="c-text__common__content">/</li>
+          <li class="c-text__common__content"><a href="#">アクセス</a></li>
+        </ul>
+      </div>
+      <div class="l-top__content md-under">
+        <a class="c-text__common__content" href="#">アップルケアネットとは</a>
+        <ul class="l-top__content__list">
+          <li class="c-text__common__content"><a href="#">代表メッセージ</a></li>
+          <li class="c-text__common__content">/</li>
+          <li class="c-text__common__content"><a href="#">企業理念</a></li>
+        </ul>
+        <ul class="l-top__content__list">
+          <li class="c-text__common__content"><a href="#">会社概要</a></li>
+          <li class="c-text__common__content">/</li>
+          <li class="c-text__common__content"><a href="#">グループ会社</a></li>
+          <li class="c-text__common__content"></li>
+        </ul>
+        <a class="c-text__common__content" href="#">アクセス</a>
+      </div>
+    </div>
+  </section>
+  <section class="l-aboutUs">
+    <div class="l-aboutUs__message">
+      <p class="c-text__single__title l-aboutUs__message__title">私たちについて<br class="md-under"><span class="c-text__single__sub-title l-aboutUs__message__sub-title">ABOUT US</span></p>
+      <p class="c-text__single__middle-title l-aboutUs__message__middle-title">あなたを見守る、<br>“地元の薬局”を提供します。</p>
+      <p class="c-text__single__post l-aboutUs__message__post">私たちが目指しているのは、地域の皆さまに愛される薬局づくり。<br>
+        「あの薬局に聞けばなんでも教えてくれる」と地域の患者さまに頼られるような機能性調剤薬局の運営を心掛けながら、
+        「薬食同源」をコンセプトに掲げたサービスの提供を行っています。</p>
+    </div>
+    <div class="l-aboutUs__img">
+      <picture>
+        <source srcset="./assets/img/common/pc-aboutUs-aboutUs.png" media="(min-width: 960px)" />
+        <source srcset="./assets/img/common/sp-aboutUs-aboutUs.png" media="(max-width: 960px)" />
+        <img src="./assets/img/common/pc-aboutUs-aboutUs.png" alt="">
+      </picture>
+    </div>
+  </section>
+  <section class="l-greeting">
+    <div class="l-greeting__label">
+      <div class="l-greeting__img">
+        <img src="./assets/img/common/aboutUs-greeting.png" alt="">
+      </div>
+      <div class="l-greeting__message">
+        <p class="c-text__single__title l-greeting__message__title">代表メッセージ<br class="md-under"><span class=" c-text__single__sub-title l-greeting__message__sub-title">GREETING</span></p>
+        <p class="c-text__single__middle-title l-greeting__message__middle-title">私たちは「信頼こそすべて」を企業理念に、
+          患者様を笑顔でお迎えし、思いやりのある誠実な姿勢で患者様中心の薬局づくりを行っています。</p>
+      </div>
+    </div>
+    <div class="l-greeting__content">
+      <p class="c-text__single__post l-greeting__content__text">西洋ではりんごを食べると病気にならないという言い伝えがあります。そこで、りんごを健康と笑顔の象徴として、社名を「アップルケアネット」とし、薬局を「アップル薬局」と名づけました。
+      </p>
+      <p class="c-text__single__post l-greeting__content__text">地域に根付いた薬局を目指し、「薬食同源」をコンセプトに、各店舗に管理栄養士を配置し、毎日の食事や栄養についても患者様にアドバイスしています。さらに、我が国が迎えようとしている超高齢化社会の実情を踏まえ、在宅医療、各種介護施設との連携など、高齢化社会に対応した新たな体制づくりも進めています。</p>
+      <p class="c-text__single__post l-greeting__content__text">江東微研グループの社訓、「足らざる点を助け合う、その和が大切」のもと、「従業員満足なくして患者様満足なし」をスローガンとして、働きやすい職場づくりをすることが患者様に満足していただける薬局につながると考えています。
+        アップルケアネットでは、「人を育て、より良い職場環境、社風をつくる」「他にはない魅力ある薬局をつくる」「感謝される仕事をする」などの行動指針を定め、各店舗、朝礼などの際に読み合わせをし、スタッフ全体の意志統一をはかっています。</p>
+      <p class="c-text__single__post l-greeting__content__text">安心して気持ち良く働ける職場があることが、スタッフにとっても患者様にとっても幸福につながるという信念のもと、明日の希望があり、永続する会社にしていきたいと思っています。</p>
+    </div>
+  </section>
+  <section class="l-direction">
+    <div class="l-direction__content">
+      <div class="l-direction__content__cell">
+        <p class="c-text__single__title l-direction__content__title">社訓<br class="md-under"><span class="c-text__single__sub-title l-direction__content__sub-title">SLOGAN</span></p>
+        <div class="l-direction__content__message">
+          <p class="c-text__single__post">足らざる点を助け合うその和が大切</p>
+        </div>
+      </div>
+      <div class="l-direction__content__cell">
+        <p class="c-text__single__title l-direction__content__title">企業理念<br class="md-under"><span class="c-text__single__sub-title l-direction__content__sub-title">CORPORATE PHILOSOPHY</span></p>
+        <div class="l-direction__content__message">
+          <p class="c-text__single__post">私たちは、「信頼こそすべて」を企業理念に、次の事項を実行します。</p>
+          <p class="c-text__single__post">私たちアップルケアネットは、薬局を通し患者様の健康と安心をサポートします。</p>
+          <p class="c-text__single__post">私たちは、江東微研グループの一員として地域社会に貢献し、地域に根差した薬局を目指します。</p>
+          <p class="c-text__single__post">私たちは、安心して働ける未来ある会社を作ります。</p>
+        </div>
+      </div>
+      <div class="l-direction__content__cell">
+        <p class="c-text__single__title l-direction__content__title">行動指針<br class="md-under"><span class="c-text__single__sub-title l-direction__content__sub-title">ACTION GUIDELINE</span></p>
+        <div class="l-direction__content__message">
+          <p class="c-text__single__post l-direction__content__message--line">1.患者様と共に健康を考えることのできる薬局を目指します。</p>
+          <p class="c-text__single__post l-direction__content__message--line">2.「薬食同源」をコンセプトに多機能で、地域に根差した薬局を目指します。</p>
+          <p class="c-text__single__post l-direction__content__message--line">3.「従業員満足なくして患者様満足なし」をスローガンに、働きやすく継続する企業を目指します。</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="l-companyInfo">
+    <div class="l-companyInfo__title">
+      <p class="c-text__single__title l-companyInfo__title__main">会社概要</p>
+      <p class="c-text__single__sub-title">COMPANY INFO</p>
+    </div>
+    <div class="l-companyInfo__table">
+      <ul class="l-companyInfo__table__list">
+        <li class="l-companyInfo__table__cell">
+          <div class="l-companyInfo__table__content">
+            <div class="l-companyInfo__table__title">
+              <p class="c-text__table__title">会社概要</p>
+            </div>
+            <div>
+              <p class="c-text__table__content">株式会社アップルケアネット</p>
+            </div>
+          </div>
+        </li>
+        <li class="l-companyInfo__table__cell">
+          <div class="l-companyInfo__table__content">
+            <div class="l-companyInfo__table__title">
+              <p class="c-text__table__title">住所</p>
+            </div>
+            <div>
+              <p class="c-text__table__content">〒133-0057 <br class="md-under">東京都江戸川区西小岩5-11-25</p>
+            </div>
+          </div>
+          <div class="l-companyInfo__table__map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.596565083864!2d139.88178221514355!3d35.73613923456633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601885d9b9b003bd%3A0x405072d572491307!2z5qCq5byP5Lya56S-44Ki44OD44OX44Or44Kx44Ki44ON44OD44OI!5e0!3m2!1sja!2sjp!4v1664162081464!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </li>
+        <li class="l-companyInfo__table__cell">
+          <div class="l-companyInfo__table__content f-align__baseline">
+            <div class="l-companyInfo__table__title">
+              <p class="c-text__table__title">TEL/FAX</p>
+            </div>
+            <div>
+              <p class="c-text__table__content">TEL.(03)3650-6284</p>
+              <p class="c-text__table__content">FAX.(03)3672-6663</p>
+            </div>
+          </div>
+        </li>
+        <li class="l-companyInfo__table__cell">
+          <div class="l-companyInfo__table__content">
+            <div class="l-companyInfo__table__title">
+              <p class="c-text__table__title">資本金</p>
+            </div>
+            <div>
+              <p class="c-text__table__content">3,000,000円</p>
+            </div>
+          </div>
+        </li>
+        <li class="l-companyInfo__table__cell">
+          <div class="l-companyInfo__table__content">
+            <div class="l-companyInfo__table__title">
+              <p class="c-text__table__title">経営者</p>
+            </div>
+            <div>
+              <p class="c-text__table__content">代表取締役　小林博美</p>
+            </div>
+          </div>
+        </li>
+        <li class="l-companyInfo__table__cell">
+          <div class="l-companyInfo__table__content">
+            <div class="l-companyInfo__table__title">
+              <p class="c-text__table__title">事業内容</p>
+            </div>
+            <div>
+              <p class="c-text__table__content">保険調剤薬局の運営</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <section class="l-group">
+    <div class="l-group__title">
+      <p class="c-text__single__title  l-group__title__main">グループ会社</p>
+      <p class="c-text__single__sub-title">CORPORATE GROUP</p>
+    </div>
+    <div class="l-group__table">
+      <ul class="l-group__table__list">
+        <li class="l-group__table__cell">
+          <div class="l-group__table__title">
+            <p class="c-text__table__title">（株）江東微生物研究所</p>
+          </div>
+          <div class="l-group__table__content">
+            <p class="c-text__table__content">〒133-0057 <br class="md-under">東京都江戸川区西小岩5-18-6<br>
+              TEL（03）3672-9171（代）</p>
+          </div>
+        </li>
+        <li class="l-group__table__cell">
+          <div class="l-group__table__title">
+            <p class="c-text__table__title">（株）ジャルコ</p>
+          </div>
+          <div class="l-group__table__content">
+            <p class="c-text__table__content">〒133-0051 <br class="md-under">東京都江戸川区北小岩6-20-14<br>
+              TEL（03）3671-9565</p>
+          </div>
+        </li>
+        <li class="l-group__table__cell">
+          <div class="l-group__table__title">
+            <p class="c-text__table__title">（株）メディカル・フロンティア</p>
+          </div>
+          <div class="l-group__table__content">
+            <p class="c-text__table__content">〒133-0051 <br class="md-under">東京都江戸川区北小岩6-20-14<br>
+              TEL（03）5693-0461</p>
+          </div>
+        </li>
+        <li class="l-group__table__cell">
+          <div class="l-group__table__title">
+            <p class="c-text__table__title">（株）ランス</p>
+          </div>
+          <div class="l-group__table__content">
+            <p class="c-text__table__content">〒224-0032 <br class="md-under">神奈川県横浜市都筑区茅ヶ崎中央24-4　第6セキビル 201<br>
+              TEL（045）944-4585</p>
+          </div>
+        </li>
+        <li class="l-group__table__cell">
+          <div class="l-group__table__title">
+            <p class="c-text__table__title">（株）ジェイバック</p>
+          </div>
+          <div class="l-group__table__content">
+            <p class="c-text__table__content">〒963-8026 <br class="md-under">福島県郡山市並木3-5-10<br>
+              TEL（024）990-2268</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+</main>
+<?php include('./footer.php'); ?>
