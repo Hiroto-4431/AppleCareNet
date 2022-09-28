3834258ebc1322460ab9d6715a9afc531f88253d
Hiroto <hirotomyk8995@gmail.com>
2022-09-28 10:50:41 +0900

    creat BreadCrumb

---
+++ thanks.php
@@ -0,0 +1,35 @@
+<!-- ===== サンクスページ ===== -->
+<?php include('./header.php'); ?>
+<!-- ===== メイン ===== -->
+<main class="l-main l-aboutUs">
+  <section class="l-top">
+    <div class="l-top__main-text">
+      <h1 class="c-text__common__main-title">お問い合わせ有難うございました。</h1>
+      <p class="c-text__common__sub-title">THANK YOU.</p>
+    </div>
+  </section>
+  <section>
+    <div>
+      <div class="c-bread__whole">
+
+        <div class="c-bread__cell past__position">
+          <p class="c-bread__text c-text__bread__white">入力</p>
+          <div class="c-bread__line past__position">
+            <div class="c-bread__square past__position"></div>
+          </div>
+        </div>
+        <div class="c-bread__cell current__position">
+          <p class="c-bread__text c-text__bread__white">確認</p>
+          <div class="c-bread__line current__position">
+            <div class="c-bread__square current__position"></div>
+          </div>
+        </div>
+
+        <div class="c-bread__cell future__position">
+          <p class="c-bread__text c-text__bread__black">完了</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php include('./footer.php'); ?>
