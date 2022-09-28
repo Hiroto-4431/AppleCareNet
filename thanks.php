4b20955e8a443775528524149dfc13b2699fd267
Hiroto <hirotomyk8995@gmail.com>
2022-09-28 12:24:37 +0900

    create ThanksPage

--- thanks.php
+++ thanks.php
@@ -4,32 +4,46 @@
 <main class="l-main l-aboutUs">
   <section class="l-top">
     <div class="l-top__main-text">
-      <h1 class="c-text__common__main-title">お問い合わせ有難うございました。</h1>
+      <h1 class="c-text__common__main-title">お問い合わせ<br class="md-under">有難うございました。</h1>
       <p class="c-text__common__sub-title">THANK YOU.</p>
     </div>
   </section>
-  <section>
-    <div>
-      <div class="c-bread__whole">
+  <section class="l-content">
 
-        <div class="c-bread__cell past__position">
-          <p class="c-bread__text c-text__bread__white">入力</p>
-          <div class="c-bread__line past__position">
-            <div class="c-bread__square past__position"></div>
-          </div>
-        </div>
-        <div class="c-bread__cell current__position">
-          <p class="c-bread__text c-text__bread__white">確認</p>
-          <div class="c-bread__line current__position">
-            <div class="c-bread__square current__position"></div>
-          </div>
-        </div>
-
-        <div class="c-bread__cell future__position">
-          <p class="c-bread__text c-text__bread__black">完了</p>
+    <div class="c-bread__whole">
+      <div class="c-bread__cell past__position">
+        <p class="c-bread__text c-text__bread__white">入力</p>
+        <div class="c-bread__line past__position">
+          <div class="c-bread__square past__position"></div>
         </div>
       </div>
+      <div class="c-bread__cell past__position">
+        <p class="c-bread__text c-text__bread__white">確認</p>
+        <div class="c-bread__line past__position">
+          <div class="c-bread__square past__position"></div>
+        </div>
+      </div>
+      <div class="c-bread__cell current__position">
+        <p class="c-bread__text c-text__bread__white">完了</p>
+      </div>
     </div>
+    <div class="l-content__message">
+      <p class="c-text__common__message">この度はご応募いただき、<br class="md-under">誠にありがとうございます。<br>
+        受付完了メール（自動送信）を<br class="md-under">送信しましたので、<br class="md-under">ご確認ください。<br>
+        入力いただいた内容を元に、<br class="md-under">順次ご案内を<br class="md-under">お送りさせていただきますので、<br class="md-under">今しばらくお待ちくださいませ。</p>
+    </div>
+    <div class="l-content__point">
+      <div class="l-content__point__text">
+        <p class="c-text__common__point l-content__point__text__first-point">万が一メールが届いてない場合、以下の原因が考えられます。</p>
+        <p class="c-text__common__point l-content__point__text__main-point">
+          ・入力されたメールアドレスに誤りがある可能性があります。<br>
+          ・迷惑メールフォルダに振り分けられている可能性があります。<br>
+          ・ドメイン設定受信を行っている可能性があります。<br>
+          「d.tabuchi@tanabiki-seimen.com」のドメイン受信設定を行ってください。</p>
+        <p class="c-text__common__advice">メールが届かない場合は恐れ入りますが、再度お問い合わせください。</p>
+      </div>
+    </div>
+
   </section>
 </main>
 <?php include('./footer.php'); ?>
