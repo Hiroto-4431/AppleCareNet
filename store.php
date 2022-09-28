005981f5ddf6c0a95a8b892dea098d44ce9b1a85
Hiroto <hirotomyk8995@gmail.com>
2022-09-28 18:45:37 +0900

    create Storepage

---
+++ store.php
@@ -0,0 +1,90 @@
+<!-- ===== 店舗情報 ===== -->
+<?php include('./header.php'); ?>
+<!-- ===== メイン ===== -->
+<main class="l-main l-store">
+  <section class="l-top">
+    <div class="l-top__main-text">
+      <div class="l-top__title">
+        <h1 class="c-text__common__main-title">店舗情報</h1>
+        <p class="c-text__common__sub-title">STORE INFORMATION</p>
+      </div>
+      <div class="l-top__select">
+        <select name="store">
+          <option disabled selected class="l-top__select__option">選択してください</option>
+          <option disabled>【山形】</option>
+          <option value="">おきたま店</option>
+          <option value="">長井店</option>
+          <option value="">美女木店</option>
+          <option disabled>【宮城】</option>
+          <option value="">川平店</option>
+          <option value="">鶴ケ谷店</option>
+          <option value="">東勝山店</option>
+          <option disabled>【福島】</option>
+          <option value="">小名浜店</option>
+          <option value="">相馬店</option>
+          <option value="">長橋店</option>
+          <option value="">湯本店</option>
+          <option value="">内郷店</option>
+          <option disabled>【群馬】</option>
+          <option value="">館林店</option>
+          <option value="">太田北口店</option>
+          <option disabled>【栃木】</option>
+          <option value="">はこのもり店</option>
+          <option value="">自治医大前店</option>
+          <option disabled>【茨城】</option>
+          <option value="">霞ヶ浦店</option>
+          <option value="">水戸店</option>
+          <option disabled>【埼玉】</option>
+          <option value="">三郷店</option>
+          <option value="">松原団地店</option>
+          <option disabled>【千葉】</option>
+          <option value="">みのり台店</option>
+          <option value="">船橋店</option>
+          <option value="">大網店</option>
+          <option value="">東金店</option>
+          <option disabled>【東京】</option>
+          <option value="">まつしま店</option>
+          <option value="">高砂店</option>
+          <option value="">小岩店</option>
+          <option value="">新小岩店</option>
+          <option value="">千駄木店</option>
+          <option value="">東久留米店</option>
+          <option value="">六番町店</option>
+          <option disabled>【神奈川】</option>
+          <option value="">港南台店</option>
+        </select>
+      </div>
+    </div>
+  </section>
+  <section class="l-content">
+    <div class="l-content__all">
+      <!-- 繰り返し -->
+      <div class="l-content__card">
+        <div class="l-content__card__name">
+          <p class="l-content__card__name--main c-text__store__name--main">千駄木店</p>
+          <p class="l-content__card__name--sub c-text__store__name--sub">SENDAGI</p>
+        </div>
+        <div class="l-content__card__map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.8538330387264!2d139.755367151644!3d35.72981313481457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188dc5df60da21%3A0x6fced6798fa334a4!2z44Ki44OD44OX44Or5Y2D6aeE5pyo6Kq_5Ymk6Jas5bGA!5e0!3m2!1sja!2sjp!4v1664354376685!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="l-content__card__link">
+          <a href="#"><span class="l-content__card__detail c-text__store__detail">詳しく見る</span></a>
+        </div>
+      </div>
+      <!-- 繰り返し -->
+      <div class="l-content__card">
+        <div class="l-content__card__name">
+          <p class="l-content__card__name--main c-text__store__name--main">千駄木店</p>
+          <p class="l-content__card__name--sub c-text__store__name--sub">SENDAGI</p>
+        </div>
+        <div class="l-content__card__map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.8538330387264!2d139.755367151644!3d35.72981313481457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188dc5df60da21%3A0x6fced6798fa334a4!2z44Ki44OD44OX44Or5Y2D6aeE5pyo6Kq_5Ymk6Jas5bGA!5e0!3m2!1sja!2sjp!4v1664354376685!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="l-content__card__link">
+          <a href="#"><span class="l-content__card__detail c-text__store__detail">詳しく見る</span></a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php include('./footer.php'); ?>
