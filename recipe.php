d397501f415db2e3b726947fa26692faa9458c69
Hiroto <hirotomyk8995@gmail.com>
2022-10-10 16:52:30 +0900

    create Recipe Page

---
+++ recipe.php
@@ -0,0 +1,113 @@
+<!-- ===== すこやか通信　===== -->
+<?php include('./header.php'); ?>
+<!-- ===== メイン ===== -->
+<main class="l-main l-recipe">
+  <section class="l-top">
+    <div class="l-top__main-text">
+      <div class="l-top__title">
+        <h1 class="c-text__common__main-title">すこやか通信</h1>
+        <p class="c-text__common__sub-title">RECIPE</p>
+      </div>
+    </div>
+  </section>
+  <section class="l-content">
+    <div class="l-content__terms">
+      <div class="l-content__terms__upper">
+        <div class="l-content__select">
+          <select name="" id="">
+            <option disabled selected>時期から探す</option>
+          </select>
+        </div>
+        <div class="l-content__select">
+          <select name="" id="">
+            <option disabled selected>タグから探す</option>
+          </select>
+        </div>
+      </div>
+      <div class="l-content__terms__under">
+        <div class="l-content__long-select">
+          <select name="" id="">
+            <option disabled selected>キーワードから探す</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div class="l-content__articles">
+      <div class="l-content__cell">
+        <a href="#">
+          <div class="l-content__cell__img">
+            <img src="./assets/img/common/recipe-05.png" alt="">
+          </div>
+          <div class="l-content__cell__date">
+            <span class="c-text__news__date">2022.03.03</span>
+          </div>
+          <div class="l-content__cell__message">
+            <p class="c-text__news__post">この文章はダミーです。文字の大きさ、量行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量行間等を確認するために入れています。この文章はダミーです。</p>
+          </div>
+          <div class="l-content__cell__tag">
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+          </div>
+        </a>
+      </div>
+      <div class="l-content__cell">
+        <a href="#">
+          <div class="l-content__cell__img">
+            <img src="./assets/img/common/recipe-05.png" alt="">
+          </div>
+          <div class="l-content__cell__date">
+            <span class="c-text__news__date">2022.03.03</span>
+          </div>
+          <div class="l-content__cell__message">
+            <p class="c-text__news__post">この文章はダミーです。文字の大きさ、量行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量行間等を確認するために入れています。この文章はダミーです。</p>
+          </div>
+          <div class="l-content__cell__tag">
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+          </div>
+        </a>
+      </div>
+      <div class="l-content__cell">
+        <a href="#">
+          <div class="l-content__cell__img">
+            <img src="./assets/img/common/recipe-05.png" alt="">
+          </div>
+          <div class="l-content__cell__date">
+            <span class="c-text__news__date">2022.03.03</span>
+          </div>
+          <div class="l-content__cell__message">
+            <p class="c-text__news__post">この文章はダミーです。文字の大きさ、量行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量行間等を確認するために入れています。この文章はダミーです。</p>
+          </div>
+          <div class="l-content__cell__tag">
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+            <span class="c-tag">
+              <p class="c-tag__text">タグサンプル1</p>
+            </span>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+  </section>
+</main>
+<?php include('./footer.php'); ?>
